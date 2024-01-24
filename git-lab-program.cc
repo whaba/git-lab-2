786f1bd8da4b6c0fa211d87d835c9411219de977
Cooper Brown <cb432122@ohio.edu>
2024-01-24 18:18:39 -0500

    struggling

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Cooper Brown>
+ *        Date: <1\24\24>
+ * Description: Git introductions
+ 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
